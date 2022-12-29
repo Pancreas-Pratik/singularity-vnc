ee59ce254f045c5dd2761c0a7e9a2574a271d08a
Pratik Mehta <71743538+Pancreas-Pratik@users.noreply.github.com>
2022-12-29 00:10:01 -0500

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -5,6 +5,7 @@ set -e
 echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y vim wget net-tools locales bzip2 \
+    python3 python3-pip python3-dev \
     python-numpy #used for websockify/novnc
 apt-get clean -y
 
